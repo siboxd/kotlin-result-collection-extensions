a6f179aea4eceb12b4866c3378f086ba1ada6fc6
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2025-04-22 21:55:02 +0200

    chore: FlowResultExtensions javadoc changes

--- flow-result-ktx/src/main/kotlin/com/github/siboxd/kotlin/extension/flow/result/FlowResultExtensions.kt
+++ flow-result-ktx/src/main/kotlin/com/github/siboxd/kotlin/extension/flow/result/FlowResultExtensions.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.*
  * Applies [transform] function to each [Result] encapsulated value of the given flow.
  *
  * The receiver of the [transform] is [FlowCollector] and thus [transform] is a
- * flexible function that may transform emitted element, skip it or emit it multiple times.
+ * flexible function that may transform the emitted element, skip it or emit it multiple times.
  *
  * This operator generalizes [filter] and [map] operators and
  * can be used as a building block for other operators.
@@ -33,7 +33,7 @@ inline fun <T, R> Flow<Result<T>>.transformResult(
  * making the flow emit every failure and continue until the next `Result.success` to be actually transformed trough [transform].
  *
  * The receiver of the [transform] is [FlowCollector] and thus [transform] is a
- * flexible function that may transform emitted element, skip it or emit it multiple times.
+ * flexible function that may transform the emitted element, skip it or emit it multiple times.
  *
  * This operator generalizes [takeWhile] and can be used as a building block for other operators.
  *
@@ -59,89 +59,67 @@ inline fun <T, R> Flow<Result<T>>.transformResultWhile(
  */
 suspend fun <T> FlowCollector<Result<T>>.emitResult(value: T) = emit(Result.success(value))
 
-/**
- * Returns a flow containing only the encapsulated values of the original flow that match the given [predicate]. Failures will still pass.
- */
-inline fun <T> Flow<Result<T>>.filterResult(crossinline predicate: suspend (T) -> Boolean): Flow<Result<T>> =
-    transformResult { value ->
-        if (predicate(value)) return@transformResult emitResult(value)
-    }
+/** Returns a flow containing only the encapsulated values of the original flow that match the given [predicate]. Failures will still pass. */
+inline fun <T> Flow<Result<T>>.filterResult(
+    crossinline predicate: suspend (T) -> Boolean
+): Flow<Result<T>> = transformResult { value ->
+    if (predicate(value)) return@transformResult emitResult(value)
+}
 
-/**
- * Returns a flow containing only the encapsulated values of the original flow that do not match the given [predicate].
- */
-inline fun <T> Flow<Result<T>>.filterResultNot(crossinline predicate: suspend (T) -> Boolean): Flow<Result<T>> =
-    transformResult { value ->
-        if (!predicate(value)) return@transformResult emitResult(value)
-    }
+/** Returns a flow containing only the encapsulated values of the original flow that do not match the given [predicate]. */
+inline fun <T> Flow<Result<T>>.filterResultNot(
+    crossinline predicate: suspend (T) -> Boolean
+): Flow<Result<T>> = transformResult { value ->
+    if (!predicate(value)) return@transformResult emitResult(value)
+}
 
-/**
- * Returns a flow containing only the encapsulated values that are instances of specified type [R].
- */
+/** Returns a flow containing only the encapsulated values that are instances of specified type [R]. */
 @Suppress("UNCHECKED_CAST")
 inline fun <reified R> Flow<Result<*>>.filterResultIsInstance(): Flow<Result<R>> =
     filterResult { it is R } as Flow<Result<R>>
 
-/**
- * Returns a flow containing only the encapsulated values of the original flow that are not null.
- */
+/** Returns a flow containing only the encapsulated values of the original flow that are not null. */
 fun <T : Any> Flow<Result<T?>>.filterResultNotNull(): Flow<Result<T>> =
-    transformResult<T?, T> { value ->
+    transformResult { value ->
         if (value != null) return@transformResult emitResult(value)
     }
 
-/**
- * Returns a flow containing the encapsulated results of applying the given [transform] function to each [Result] encapsulated value of the original flow.
- */
+/** Returns a flow containing the encapsulated results of applying the given [transform] function to each [Result] encapsulated value of the original flow. */
 inline fun <T, R> Flow<Result<T>>.mapResult(
     crossinline transform: suspend (value: T) -> R
 ): Flow<Result<R>> = map { result -> result.map { transform(it) } }
 
-/**
- * Returns a flow containing the encapsulated results of applying the given [transform] function to each [Result] encapsulated value of the original flow, catching thrown exceptions.
- */
+/** Returns a flow containing the encapsulated results of applying the given [transform] function to each [Result] encapsulated value of the original flow, catching thrown exceptions. */
 inline fun <T, R> Flow<Result<T>>.mapResultCatching(
     crossinline transform: suspend (value: T) -> R
 ): Flow<Result<R>> = map { result -> result.mapCatching { transform(it) } }
 
-/**
- * Returns a flow containing the results of applying the given [transform] function to each [Result] encapsulated value of the original flow.
- */
+/** Returns a flow containing the results of applying the given [transform] function to each [Result] encapsulated value of the original flow. */
 inline fun <T, R> Flow<Result<T>>.flatMapResult(
     crossinline transform: suspend (value: T) -> Result<R>
 ): Flow<Result<R>> = map { result -> result.flatMap { transform(it) } }
 
-/**
- * Returns a flow containing the results of applying the given [transform] function to each [Result] encapsulated value of the original flow, catching thrown exceptions.
- */
+/** Returns a flow containing the results of applying the given [transform] function to each [Result] encapsulated value of the original flow, catching thrown exceptions. */
 inline fun <T, R> Flow<Result<T>>.flatMapResultCatching(
     crossinline transform: suspend (value: T) -> Result<R>
 ): Flow<Result<R>> = map { result -> result.flatMapCatching { transform(it) } }
 
-/**
- * Returns a flow that invokes the given [action] on successful results **before** each value of the upstream flow is emitted downstream.
- */
+/** Returns a flow that invokes the given [action] on successful results **before** each value of the upstream flow is emitted downstream. */
 inline fun <T> Flow<Result<T>>.onEachResultSuccess(
     crossinline action: suspend (value: T) -> Unit
 ): Flow<Result<T>> = onEach { result -> result.onSuccess { action(it) } }
 
-/**
- * Returns a flow that invokes the given [action] on failure results **before** each value of the upstream flow is emitted downstream.
- */
+/** Returns a flow that invokes the given [action] on failure results **before** each value of the upstream flow is emitted downstream. */
 inline fun <T> Flow<Result<T>>.onEachResultFailure(
     crossinline action: suspend (value: Throwable) -> Unit
 ): Flow<Result<T>> = onEach { result -> result.onFailure { action(it) } }
 
-/**
- * Returns a flow containing the flow of encapsulated results after applying the given [transform] function to each [Result] encapsulated value of the original flow of flows, catching thrown exceptions.
- */
+/** Returns a flow containing the flow of encapsulated results after applying the given [transform] function to each [Result] encapsulated value of the original flow of flows, catching thrown exceptions. */
 inline fun <T, R> Flow<Flow<Result<T>>>.mapInnerResults(
     crossinline transform: suspend (value: T) -> R
 ): Flow<Flow<Result<R>>> = map { innerFlow -> innerFlow.mapResultCatching(transform) }
 
-/**
- * Returns a flow containing the flow of results after applying the given [transform] function to each [Result] encapsulated value of the original flow of flows, catching thrown exceptions.
- */
+/** Returns a flow containing the flow of results after applying the given [transform] function to each [Result] encapsulated value of the original flow of flows, catching thrown exceptions. */
 inline fun <T, R> Flow<Flow<Result<T>>>.flatMapInnerResults(
     crossinline transform: suspend (value: T) -> Result<R>
 ): Flow<Flow<Result<R>>> = map { innerFlow -> innerFlow.flatMapResultCatching(transform) }
